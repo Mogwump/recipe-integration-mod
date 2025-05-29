c0f8492b77f7c134ff35566fb17de29434d4b0da
Mogwump <136357423+Mogwump@users.noreply.github.com>
2025-05-29 18:22:01 +0100

    initial commit

---
+++ src/main/java/mogwump/recipe_integration/RecipeIntegration.java
@@ -0,0 +1,9 @@
+package mogwump.recipe_integration;
+
+public final class RecipeIntegration {
+    public static final String MOD_ID = "recipe_integration";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
